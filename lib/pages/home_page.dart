116ded3a398d149a31d7cc831e5170990ad82108
mars985 <clasherprofile.3@gmail.com>
2024-01-04 18:47:35 +0530

    learned some navigation and how to use basic widgets like rows, containers and decoration boxes

---
+++ lib/pages/home_page.dart
@@ -0,0 +1,40 @@
+import 'package:flutter/material.dart';
+
+class HomePage extends StatefulWidget {
+  const HomePage({super.key});
+
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage>
+    with SingleTickerProviderStateMixin {
+  late AnimationController _controller;
+
+  @override
+  void initState() {
+    super.initState();
+    _controller = AnimationController(vsync: this);
+  }
+
+  @override
+  void dispose() {
+    _controller.dispose();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: Text("Home Page"),),
+      body: Center(
+        child: ElevatedButton(
+          child: Text("Page 2!"),
+          onPressed: () {
+            Navigator.pushNamed(context, '/secondpage');
+          },
+        ),
+      ),
+    );
+  }
+}
