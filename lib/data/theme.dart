37633a0717f486b5fb3aef2ab6665c4cc64bd78d
mars985 <clasherprofile.3@gmail.com>
2024-02-04 23:06:41 +0530

    rudimentary page navigation completed. set up drawer menu

--- lib/data/theme.dart
+++ lib/data/theme.dart
@@ -1,31 +1,6 @@
 import 'dart:ui';
 
 class Themes {
-  List colorPalette = [
-    /*
-      background,
-      text,
-      primary,
-      secondary,
-      accent,
-    */
-    // [
-    //   Color(0xFF0a0616),
-    //   Color(0xFFe1faea),
-    //   Color(0xFFaa96e6),
-    //   Color(0xFF31118f),
-    //   Color(0xFFd13367),
-    // ],
-    [
-      0xFF041a0c,
-      0xFFe1faea,
-      0xFF8bedac,
-      0xFF16718d,
-      0xFF4284e1,
-      0xFF1F1F1F,
-    ],
-  ];
-
   List colors = [
     {
       "background": 0xFFEFEFEF,
@@ -51,12 +26,13 @@ class Themes {
       "text_light": 0xFF4284e1,
       "accent": 0xFF1F1F1F,
     },
-  ];
-}
-
+    // {
       // "background": 
       // "primary": 
       // "secondary": 
       // "text_dark": 
       // "text_light": 
       // "accent": 
+    // },
+  ];
+}
--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -3,6 +3,7 @@ import 'package:google_nav_bar/google_nav_bar.dart';
 import 'package:hive_flutter/hive_flutter.dart';
 import 'package:juno/data/database.dart';
 import 'package:juno/data/theme.dart';
+import 'package:juno/models/navbar.dart';
 
 class HomePage extends StatefulWidget {
   const HomePage({super.key});
@@ -24,51 +25,41 @@ class _HomePageState extends State<HomePage>
     _controller = AnimationController(vsync: this);
   }
 
-  @override
-  void dispose() {
-    _controller.dispose();
-    super.dispose();
-  }
+  // @override
+  // void dispose() {
+  //   _controller.dispose();
+  //   super.dispose();
+  // }
 
   var _textController = TextEditingController();
+
+  var themePicked = 0;
   Themes theme = Themes();
-  var themePicked = 1;
   getColor(paletteName) {
     // return theme.colorPalette[themePicked][index];
     return Color(theme.colors[themePicked][paletteName]);
   }
 
+  var _selectedIndex = 0;
+
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      backgroundColor: getColor("background"),
-      appBar: AppBar(
-        title: Text(
-          "Expenses",
+    return Column(
+      // crossAxisAlignment: CrossAxisAlignment.start,
+      children: [
+        // placeholder
+        Text(
+          "home",
           style: TextStyle(color: getColor("text_dark")),
         ),
-        backgroundColor: getColor("background"),
-      ),
-      body: Text(
-        "hello",
-        style: TextStyle(color: getColor("text_dark")),
-      ),
-      bottomNavigationBar: Container(
-        color: getColor("primary"),
-        child: GNav(
-          // padding: EdgeInsets.all(10),
-          iconSize: 24,
-          tabBackgroundColor: getColor("accent"),
-          tabBorderRadius: 20,
-          // padding: EdgeInsets.all(20),
-          tabs: [
-            GButton(
-                iconActiveColor: getColor("secondary"),
-                icon: Icons.home,
-                text: "H O M E"),
-          ],
-        ),
-      ),
+        Container(
+          child: Text("container"),
+          color: Colors.amber[800],
+          width: 100,
+          height: 100,
+        )
+      ],
     );
+    // return Placeholder();
   }
 }
