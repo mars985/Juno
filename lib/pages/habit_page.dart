f03d1e4630fa4c133692828bb48c6db66bfa727d
mars985 <clasherprofile.3@gmail.com>
2024-02-11 16:09:46 +0530

    added habits database (see todo.txt), appbar text list

--- lib/pages/habit_page.dart
+++ lib/pages/habit_page.dart
@@ -5,27 +5,53 @@ class HabitPage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return ListView.builder(
-      itemCount: 5,
-      itemBuilder: (BuildContext context, int index) {
-        return ListTile(
-          leading: Checkbox(
-            value: false,
-            onChanged: (value) {},
-            
-          ),
-          title: Column(
-            // mainAxisAlignment: MainAxisAlignment.start,
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: [
-              Text("Work out", style: TextStyle(fontSize: 16),),
-              Text("07:30", style: TextStyle(fontSize: 12, fontWeight: FontWeight.w400),)
-            ],
-          ),
-        );
-      },
-      scrollDirection: Axis.vertical,
-      shrinkWrap: true,
+    // return ListView(
+    //   shrinkWrap: true,
+    //   scrollDirection: Axis.vertical,
+    //   children: [
+    //     ListView.builder(
+    //       itemCount: 5,
+    //       itemBuilder: (BuildContext context, int index) {
+    //         return ListTile(
+    //           leading: Checkbox(
+    //             value: false,
+    //             onChanged: (value) {},
+    //           ),
+    //           title: Expanded(
+    //             child: Column(
+    //               crossAxisAlignment: CrossAxisAlignment.start,
+    //               children: [
+    //                 Text(
+    //                   "Work out",
+    //                   style: TextStyle(fontSize: 16),
+    //                 ),
+    //                 Text(
+    //                   "07:30",
+    //                   style: TextStyle(fontSize: 12, fontWeight: FontWeight.w400),
+    //                 )
+    //               ],
+    //             ),
+    //           ),
+    //         );
+    //       },
+    //       scrollDirection: Axis.vertical,
+    //       shrinkWrap: true,
+    //     ),
+    //   ],
+    // );
+
+    return Column(
+      children: [
+        ListView.builder(
+          shrinkWrap: true,
+          itemCount: 5,
+          itemBuilder: (BuildContext context, int index) {
+            return ListTile(
+              title: Text("data 2"),
+            );
+          },
+        ),
+      ],
     );
   }
 }
